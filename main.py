07f1b7d1053b9736d765834d43c04f26ea86f237
allvolload <juniormiddlehead@yandex.ru>
2023-02-28 01:03:50 +0300

    updates for mac edit writing cycle (delete bag recreating new file)

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import random
 import platform
 from tempfile import TemporaryDirectory
 from pathlib import Path
@@ -7,37 +6,54 @@ from pdf2image import convert_from_path
 from PIL import Image
 
 
-
+ur_path_to_new_file = "name_file.txt"
 PDF_file = Path(r"d_big_100p.pdf")
 page_start = int(input('Input START page number :   '))
 page_end = int(input('Input END page number   :   '))
 language = str(input('Input LANGUAGE in PDF (rus/eng/fra)  :   '))
+
+
 image_file_list = []
 
 
 if platform.system() == "Windows":
-    print('[1/5] win system was find')
+    print('[1/4] win system was find')
 
     pytesseract.pytesseract.tesseract_cmd = (r"C:\Program Files\Tesseract-OCR\tesseract.exe")
-    print('[2/5] tesseract was find')
+    print('[2/4] tesseract was find')
 
     path_to_poppler_exe = Path(r"C:\poppler-23.01.0\Library\bin")
-    print('[3/5] poppler was find')
+    print('[3/4] poppler was find')
 
     out_directory = Path(r"~\Desktop").expanduser()
+
+if platform.system() == "Darwin":
+    print('[1/4] mac system detected')
+
+    pytesseract.pytesseract.tesseract_cmd = (r"/usr/local/bin/tesseract")
+    print('[2/4] tesseract was find')
+
+    path_to_poppler_exe = Path(r"/usr/local/bin/poppler-23.01.0/Library/bin")
+    print('[3/4] poppler was find')
+
+    out_directory = Path('~/Desktop').expanduser()
+
 else:
-    out_directory = Path("~").expanduser()
+    print('Your system not supported')
+    exit()
 
 
-text_file = out_directory / Path("out_text_" + str(random.randint(1, 10)) + ".txt")  # defense rewriting
+text_file = out_directory / Path(ur_path_to_new_file)
 
 
 def main(here_start, here_end, language_in_pdf):
 
+    elem_num = 0
     count = here_start
 
     with TemporaryDirectory() as tempdir:
 
+        print('[4/4] convertation cycle to poppler was start')
         if platform.system() == "Windows":
             pdf_pages = convert_from_path(
                 PDF_file,
@@ -48,7 +64,6 @@ def main(here_start, here_end, language_in_pdf):
             )
         else:
             pdf_pages = convert_from_path(PDF_file, 500)
-        print('[4/5] cycle convertating to poppler was start')
 
         for page_enumeration, page in enumerate(pdf_pages, start=1):
 
@@ -58,22 +73,25 @@ def main(here_start, here_end, language_in_pdf):
 
                 image_file_list.append(filename)
 
-        print('[5/5] cycle convertating img-txt was start')
-        with open(text_file, "w", encoding='utf-8') as output_file:
+                with open(text_file, "a", encoding='utf-8') as output_file:
 
-            for image_file in image_file_list:
+                    image_file = image_file_list[elem_num]
 
-                print(f"Page {count} is ready")
+                    print(f"Page {count} is ready")
 
-                text = str(pytesseract.image_to_string(Image.open(image_file), lang=language_in_pdf))
+                    text = str(pytesseract.image_to_string(Image.open(image_file), lang=language_in_pdf))
 
-                text = text.replace("-\n", "")
+                    text = text.replace("-\n", "")
 
-                output_file.write(text)
+                    output_file.write(text)
 
-                count += 1
+                    elem_num += 1
+
+                    count += 1
+        print('READY')
 
 
 if __name__ == "__main__":
 
     main(page_start, page_end, language)
+
